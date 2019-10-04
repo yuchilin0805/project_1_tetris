b7bad94cfd3c88249f5b05c22368c6184b0bf061
林鈺期 <dog0805@gmail.com>
2019-10-05 01:31:14 +0800

    ver0 set shape

--- tetris.cpp
+++ tetris.cpp
@@ -1,34 +1,92 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstring>
 using namespace std;
 class cmd{
-    public:
+    public: 
+        int rows;
+        int cols;       
         string cmds;
         int col_pos;
+};
+class blocks{
+    public:
+        blocks(string &t){
+            type=t;
+            if(type=="T1")
+                strncpy(shape,"nrqrr",5);
+            else if(type=="L1")
+                strncpy(shape,"yrqu",5);     //n y q r u l
+            else if(type=="J1")
+                strncpy(shape,"yruu",5);
+            else if(type=="S1")
+                strncpy(shape,"yrur",5);
+            else if(type=="I1")
+                strncpy(shape,"yuuu",5);
+            else if(type=="T2")
+                strncpy(shape,"nrqru",5);
+            else if(type=="L2")
+                strncpy(shape,"yurr",5);
+            else if(type=="J2")
+                strncpy(shape,"yrrql",5);
+            else if(type=="S2")
+                strncpy(shape,"nrqrq",5);
+            else if(type=="I2")
+                strncpy(shape,"yrrr",5);
+            else if(type=="T3")
+                strncpy(shape,"yrrq",5);
+            else if(type=="L3")
+                strncpy(shape,"nruul",5);
+            else if(type=="J3")
+                strncpy(shape,"yuur",5);
+            else if(type=="Z1")
+                strncpy(shape,"nrrql",5);
+            else if(type=="O")
+                strncpy(shape,"yrqr",5);
+            else if(type=="T4")
+                strncpy(shape,"yurq",5);
+            else if(type=="L4")
+                strncpy(shape,"yrru",5);
+            else if(type=="J4")        //    ffffffffffffffffff
+                strncpy(shape,"f",5);
+            else if(type=="Z2")
+                strncpy(shape,"yuru",5);
+            cout<<shape;
+
+        }
+    private:
+        char shape[5];
+        string type;
 
 };
-
+int read_file(cmd* command){
+    int flag=0,i=0;
+    ifstream fin("tetris.data.txt");  
+    do{
+        if(!flag){
+            fin>>command[i].rows>>command[i].cols;
+            flag=1;
+        }
+        fin>>command[i].cmds>>command[i].col_pos;
+        if(command[i].cmds=="End")
+            break;
+        i++;
+    }while(!fin.eof());
+    
+    fin.close();
+    return i;
+}
 int main(){
-  int rows,cols,i=0;
-  char s[100][100];
-  int flag=0;
-  ifstream fin("tetris.data.txt");
-
-  cmd command[100];
-  do{
-    if(!flag){
-        fin>>rows>>cols;
-        flag=1;
-    }
-    fin>>command[i].cmds>>command[i].col_pos;
-    if(command[i].cmds=="End")
-        break;
-    i++;
-  }while(!fin.eof());
-
-    for(int j=0;j<i;j++){
+    int rows,cols;
+    int flag=0;
+    cmd command[1005];
+    int cmd_lenth=read_file(command);
+    blocks k(command[3].cmds);
+    /*cout<<cmd_lenth;
+    for(int j=0;j<cmd_lenth;j++){
         cout<<command[j].cmds<<" "<<command[j].col_pos<<endl;
-    }
-fin.close();
+    }*/
+
 return 0;
 }
